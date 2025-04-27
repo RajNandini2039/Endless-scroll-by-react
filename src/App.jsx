9b1411bdc9869ec1b6acd48fedb9cda2482d24ae
RajNandini2039 <rajnandini2039@gmail.com>
2025-04-27 21:14:46 +0530

    created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import Infinite from "./Infinite.jsx";
+import Infinite from "./Infinite";
 
 import './App.css'
 
--- src/Infinite.jsx
+++ src/Infinite.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from "react";
-import './Infinite.css';
+import './infinite.css';
 
 
 
